0d85eedd64dcc5d2a42ea1e58c2ea0598eedf28a
alexfaus08 <alexfaus08@gmail.com>
2018-11-02 12:13:49 -0400

    Movement now works for arrow keys

--- Player.gd
+++ Player.gd
@@ -7,14 +7,18 @@ func _ready():
     screensize = get_viewport_rect().size
 	
 func _process(delta):
-    var velocity = Vector2() # The player's movement vector.
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
+	var velocity = Vector2() # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	# Clamping a value means restricting it to a given range.
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
