829d0efb4b31e9f804059d746856a1f2dffb5c2b
alexfaus08 <alexfaus08@gmail.com>
2018-11-02 16:40:18 -0400

    Cleanup

--- Player.gd
+++ Player.gd
@@ -9,22 +9,20 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	set_physics_process(true)
-	# RayNode = get_node("RayCast2D")
 	pass
 	
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_up"):
 		motion.y = -motion_speed 
-		# RayNode.set_rotd(180)
 		
 	if Input.is_action_pressed("ui_down"):
 		motion.y = motion_speed
-		# RayNode.set_rotd(0)	
+
 	
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -motion_speed
-		# RayNode.set_rotd(-90)	
+	
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = motion_speed
