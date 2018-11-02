df8d4d9413d8c0cc6980ba5157bc02142f482055
alexfaus08 <alexfaus08@gmail.com>
2018-11-02 11:58:41 -0400

    Added up and down movements for Player
    movement according the the arrow keys

--- Player.gd
+++ Player.gd
@@ -9,4 +9,13 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -100
 		move_and_slide(motion)
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = -100
+		move_and_slide(motion)
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = 100
+		move_and_slide(motion)
+	else:
+		motion.x = 0
+		motion.y = 0
 	pass
