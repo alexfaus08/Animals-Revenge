c47fe488a103ff62f0b343ad49a0d0fb4037b163
alexfaus08 <alexfaus08@gmail.com>
2018-11-03 13:48:06 -0400

    Left and Right Movement Work
    Turkey animates left and right. When not moving left and right, is idle. However, Up and Down animations are not working

--- Player.gd
+++ Player.gd
@@ -13,20 +13,31 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_up"):
 		motion.y = -motion_speed 
+		$AnimatedSprite.play("Walk Up")
 		
 	elif Input.is_action_pressed("ui_down"):
 		motion.y = motion_speed
+		$AnimatedSprite.play("Walk Up")
 	
 	else:
 		motion.y = 0
+		if(Input.is_action_just_released("ui_up")):
+			$AnimatedSprite.play("Idle Up")
+		elif(Input.is_action_just_released("ui_down")):
+			$AnimatedSprite.play("Idle Down")
 	
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -motion_speed
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Walk Right")
 	
 	elif Input.is_action_pressed("ui_right"):
 		motion.x = motion_speed
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Walk Right")
 	
 	else:
 		motion.x = 0
+		$AnimatedSprite.play("Idle Right")
 	
 	move_and_slide(motion)
