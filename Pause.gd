4f939ed5ab441860b24b5095b24b38ec9448d036
alexfaus08 <alexfaus08@gmail.com>
2018-11-03 22:49:05 -0400

    Pause game allowed
    Game can now be paused by pressing P

--- Pause.gd
+++ Pause.gd
@@ -1,4 +1,12 @@
 extends Control
 
 func _ready():
-	pass
+	hide()
+
+func _input(event):
+	# pauses all proccesses
+	if (event.is_action_pressed("pause")):
+		print("Pause")
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state 
