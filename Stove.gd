b61236440945b1ee1f3e93d080f0694a287749a5
alexfaus08 <alexfaus08@gmail.com>
2018-11-03 20:19:49 -0400

    Stove now animates for 5 seconds
    stove animates and then stops after 5 seconds have passed

--- Stove.gd
+++ Stove.gd
@@ -1,5 +1,6 @@
 extends Area2D
 var player_near 
+onready var t = get_node("../Timer")
 
 func _ready():
 	$StoveSprite.play("Idle")
@@ -8,6 +9,15 @@ func _process(delta):
 	if(player_near):
 		if(Input.is_action_just_pressed("ui_select")):
 			$StoveSprite.play("Cooking")
+			# wait 5 seconds
+			t.set_wait_time(5)
+			t.start()
+			yield(t, "timeout")
+			$StoveSprite.play("On")
+			t.set_wait_time(3)
+			t.start()
+			yield(t, "timeout")
+			$StoveSprite.play("Idle")
 
 func _on_Stove_body_entered(body):
 	if(body.get_name() == "Player"):
