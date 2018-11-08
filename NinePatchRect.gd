81ea7c09d0b6ff60b1f83ff5b241514375c64c0d
alexfaus08 <alexfaus08@gmail.com>
2018-11-08 17:03:44 -0500

    added island

--- NinePatchRect.gd
+++ NinePatchRect.gd
@@ -7,7 +7,7 @@ func _process(delta):
 	if(Fridge.player_near_fridge == false):
 		hide()
 
-
-func _on_Potato_pressed():
-	Player.add_object("Potatoes")
+func _on_Milk_pressed():
+	Player.add_object("Milk")
 	hide()
+
