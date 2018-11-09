c2aadf2dabb5d61abe5097c84842ee4573ca8ac6
alexfaus08 <alexfaus08@gmail.com>
2018-11-09 11:45:43 -0500

    Gave pop up buttons signals to add food to player holding

--- NinePatchRect.gd
+++ NinePatchRect.gd
@@ -11,3 +11,25 @@ func _on_Milk_pressed():
 	Player.add_object("Milk")
 	hide()
 
+
+
+func _on_Butter_pressed():
+	Player.add_object("Butter")
+	hide()
+	
+	
+
+
+func _on_Turkey_pressed():
+	Player.add_object("Turkey")
+	hide()
+
+
+func _on_Chicken_pressed():
+	Player.add_object("Chicken")
+	hide()
+
+
+func _on_Cheese_pressed():
+	Player.add_object("Cheese")
+	hide()
