a856bae3e3b3df0a30586b3363208117f3ba595b
Katarina Sperduto <katarinasperduto@bellsouth.net>
2018-11-11 17:48:45 -0500

    SlideFunction

--- Window.gd
+++ Window.gd
@@ -4,12 +4,27 @@ extends Area2D
 
 onready var world = $"../../../World"
 onready var popup = $"../../Order"
+onready var turkey = $Turkey
+onready var Recipe2 = $"../../Recipe2"
+onready var Recipe3 = $"../../Recipe3"
+onready var Recipe4 = $"../../Recipe4"
+onready var slide1 = $"../../Recipe2/slide1"
+onready var slide2 = $"../../Recipe3/slide2"
+onready var slide3 = $"../../Recipe4/slide3"
+#onready var animation = $"../../slide"
+var recipe1 = false
+var recipe2 = false
+var recipe3 = false
+var order
 var player_near_window
 var dishes = []
 var side_dishes = []
 #var diag = AcceptDialog.new()
 
 func _ready():
+	Recipe2.hide()
+	Recipe3.hide()
+	Recipe4.hide()
 	player_near_window = false
 	world._ready()
 	for x in world.get_recipes():
@@ -34,23 +49,50 @@ func _recipes_Sides():
 
 func _process(delta):
 	if(player_near_window):
+		var main_recipe = _recipes_Main_Course()
+		var side_recipe = _recipes_Sides()
 		if(Input.is_action_just_pressed("ui_select")):
-			#print(_recipes_Main_Course().get_ingredients()) 
-			#print(_recipes_Sides().get_ingredients())
-			var main_recipe = _recipes_Main_Course()
-			var side_recipe = _recipes_Sides()
-			print(main_recipe.get_ingredients().size())
-			print(side_recipe.get_ingredients())
-			popup.get_label().text =("Main: " + main_recipe.get_ingredients()[0] + "\nSide: "+ side_recipe.get_ingredients()[0])
-			popup.show()
-			print("\n")
+			if(recipe1 == false):
+				recipe1 = true
+				Recipe2.show()
+				Recipe2.play("Appear")			
+				slide1.play("slide1")
+				print(main_recipe.get_ingredients())
+				print(side_recipe.get_ingredients())
+				print("\n")
+			elif(recipe1 == true and recipe2 == false):
+				recipe2 = true
+				Recipe3.show()
+				Recipe3.play("Appear")			
+				slide2.play("slide2")
+				print(main_recipe.get_ingredients())
+				print(side_recipe.get_ingredients())
+				print("\n")
+			elif(recipe1 == true and recipe2 == true and recipe3 == false):
+				recipe3 = true
+				Recipe4.show()
+				Recipe4.play("Appear")			
+				slide3.play("slide3")
+				print(main_recipe.get_ingredients())
+				print(side_recipe.get_ingredients())
+				print("\n")
+			elif(recipe1 == true and recipe2 == true and recipe3 == true):
+				print("Backed up orders")
+					
+			
+			
+		#else:
+			#print("too many orders on the counter")
+
 
 func _on_Window_body_entered(body):
 	if (body.get_name() == "Player"):
 		player_near_window = true
 
+	
+
 
 func _on_Window_body_exited(body):
 	if (body.get_name() == "Player"):
 		player_near_window = false
-
+		#Recipe2.hide()
