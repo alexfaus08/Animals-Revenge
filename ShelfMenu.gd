4df0d3737d1526d059b75c33c435e9df755b0919
alexfaus08 <alexfaus08@gmail.com>
2018-11-11 20:31:46 -0500

    added signals to shelf menu buttons

--- ShelfMenu.gd
+++ ShelfMenu.gd
@@ -12,3 +12,31 @@ func _on_Potato_pressed():
 	Player.add_object("Potatoes")
 	hide()
 
+
+
+func _on_Potatos_pressed():
+	pass # replace with function body
+
+
+func _on_Celery_pressed():
+	pass # replace with function body
+
+
+func _on_Corn_pressed():
+	pass # replace with function body
+
+
+func _on_Cranberries_pressed():
+	pass # replace with function body
+
+
+func _on_Bread_pressed():
+	pass # replace with function body
+
+
+func _on_Apples_pressed():
+	pass # replace with function body
+
+
+func _on_Onions_pressed():
+	pass # replace with function body
