ad0dc54b1760185fc484f87591a8ab5ca7ef6ba9
alexfaus08 <alexfaus08@gmail.com>
2018-11-15 18:59:22 -0500

    init oven recipes

--- Oven.gd
+++ Oven.gd
@@ -1,13 +1,22 @@
 extends StaticBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var oven_contents = []
+var oven_recipes = []
+var recipe_ready = false
+var recipe
+var player_near_oven = false
+var oven_off = true
+onready var player = $"../../Player"
+onready var screen_print =$"../../Bottom Box/TextPrint"
+onready var world = $"../../../World"
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	$OvenSprite.play("Idle")
+	world._ready()
+	# search for specifically stove recipes in all the recipes in the game
+	for x in world.get_recipes():
+		if(x.get_appliance() == "Oven"):
+			oven_recipes.append(x.get_ingredients())
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
