51ade8169e87131335af57862024ab9475c593bf
alexfaus08 <alexfaus08@gmail.com>
2018-11-18 13:13:11 -0500

    working on Oven mechanics

--- Oven.gd
+++ Oven.gd
@@ -1,12 +1,16 @@
 extends StaticBody2D
 
-var oven_contents = []
+var top_oven_contents = []
+var bottom_oven_contents = []
 var oven_recipes = []
 var recipe_ready = false
 var recipe
 var player_near_oven = false
 var oven_top_off = true
 var oven_bottom_off = true
+var both_on = true
+onready var top_timer = $"Top Oven Timer"
+onready var bottom_timer = $"Bottom Oven Timer"
 onready var player = $"../../Player"
 onready var screen_print =$"../../Bottom Box/TextPrint"
 onready var world = $"../../../World"
@@ -20,35 +24,30 @@ func _ready():
 			oven_recipes.append(x.get_ingredients())
 	
 func _process(delta):
-	if(player_near_oven and Input.is_action_just_pressed("ui_select")):
-		if(player.holding.size() > 0 and (oven_top_off or oven_bottom_off)):
-			oven_contents.append(player.holding.pop_front())
-			oven_contents.sort()
-			if(oven_contents in oven_recipes):
-				recipe = world.recipe_lookup(oven_contents)
-				if(oven_top_off):
-					oven_top_off = false
-					cook("top", recipe)
-					oven_top_off = true
-				elif(oven_bottom_off):
-					oven_bottom_off = false
-					cook("bottom", recipe)
-					oven_bottom_off = true
-		elif(recipe_ready):
-			player.add_object(oven_contents.pop_front())
-	elif(player_near_oven and Input.is_action_just_pressed("right_click")):
-		oven_contents.clear()
-			
-func cook(oven, recipe):
-	if(oven == "top"):
-		$OvenSprite.play("Cook Top")
-		#TODO: cook in top oven
-		pass
-	else:
-		#TODO: cook in bottom oven
-		pass
+	pass
+	# is the player near the oven and did they click left click? 
+		# are they holding an ingredient?
+			# is there an oven available?
+				# put ingredient in available oven
+				# start the timer
+	# else if the player is near and click right click?
+		# if there an oven that is off and has ingredients in it?
+			# perhaps prompt, would you like to clear the contents of: Top Oven, Bottom Oven
+	# running process to check what the ovens are doing
+		# Which timers are running?
+			# both timers running?
+				# play both animations
+			# top timer running?
+				# play top animation
+			# bottom timer running?
+				# player bottom animation
 
-# TODO:
-	# figure out how to get the bottom and top animations to overlap
-	# cook the recipe
-	# have the recipe returnable to the user
+
+
+func _on_Top_Oven_Timer_timeout():
+	oven_top_off = true
+	# add the recipe to the top oven contents and make it retrievable 
+
+
+func _on_Bottom_Oven_Timer_timeout():
+	oven_bottom_off = true
