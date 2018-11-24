405c80f251e015a5d0719bdfe94a4ad26d282708
alexfaus08 <alexfaus08@gmail.com>
2018-11-24 10:50:09 -0500

    Different poisons available

--- PoisonMenu.gd
+++ PoisonMenu.gd
@@ -1,15 +1,21 @@
 extends NinePatchRect
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var mixing_station = $"../Appliances/Mixing Station"
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _on_Slight_pressed():
+	mixing_station.which_poison(.25)
+	mixing_station.poison()
+	hide()
+
+
+func _on_Better_pressed():
+	mixing_station.which_poison(.50)
+	mixing_station.poison()
+	hide()
+
+
+func _on_Killer_pressed():
+	mixing_station.which_poison(1)
+	mixing_station.poison()
+	hide()
