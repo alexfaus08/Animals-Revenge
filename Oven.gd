7ac2534c6ea97989e7202c67728dbbcad3636c51
alexfaus08 <alexfaus08@gmail.com>
2018-11-27 16:40:33 -0500

    Oven fixed

--- Oven.gd
+++ Oven.gd
@@ -3,8 +3,10 @@ extends StaticBody2D
 var top_oven_contents = []
 var bottom_oven_contents = []
 var oven_recipes = []
-var recipe_ready = false
-var recipe
+var top_recipe_ready = false
+var bottom_recipe_ready = false
+var bottom_recipe
+var top_recipe
 var player_near_oven = false
 var top_oven_off = true
 var bottom_oven_off = true
@@ -26,48 +28,25 @@ func _process(delta):
 	# is the player near the oven and did they click left click? 
 	if(player_near_oven and Input.is_action_just_pressed("ui_select")):
 		# are they holding an ingredient?
-		if(player.holding.size() > 0):
-			if(typeof(player.holding[0]) == TYPE_STRING):
-			# is there an oven available?
-				if(top_oven_off):
-					print("stuff in oven")
-					top_oven_contents.append(player.holding.pop_front())
-				elif(bottom_oven_off):
-					print("stuff in oven")
-					bottom_oven_contents.append(player.holding.pop_front())
-		# are they holding nothing?
-		elif(recipe_ready):
-			if(recipe in top_oven_contents):
-				top_oven_contents.clear()
-			elif(recipe in bottom_oven_contents):
-				bottom_oven_contents.clear()
-			player.add_object(recipe)
-			$OvenReady.hide()
-			recipe_ready = false
-			# is there an oven that has a recipe ready?
-				# top oven has recipe ready:
-					# grab recipe from top oven 
-				# bottom has recipe:
-					# get recipe
-				# both ovens have recipe ready
-					# ??????
-					# have a pop up????
-	# else if the player is near and click right click?
-		# if there an oven that is off and has ingredients in it?
-			# perhaps prompt, would you like to clear the contents of: Top Oven, Bottom Oven
+		$"Control/OvenPopUp".show()
+	if(not player_near_oven):
+		$"Control/OvenPopUp".hide()
+	if(top_recipe_ready or bottom_recipe_ready):
+		$OvenReady.show()
+		$OvenReady.play("ready")
 	# running process to check what the ovens are doing
 	top_oven_contents.sort()
 	bottom_oven_contents.sort()
 	# do any of the oven contents match a recipe?
-	if(top_oven_contents in oven_recipes and top_oven_off and not recipe_ready):
+	if(top_oven_contents in oven_recipes and top_oven_off and not top_recipe_ready):
 		# the top oven has a valid recipe
-		recipe = world.recipe_lookup(top_oven_contents)
-		top_timer.set_wait_time(recipe.get_cook_time())
+		top_recipe = world.recipe_lookup(top_oven_contents)
+		top_timer.set_wait_time(top_recipe.get_cook_time())
 		top_timer.start()
 		top_oven_off = false
-	elif(bottom_oven_contents in oven_recipes and bottom_oven_off and not recipe_ready):
-		recipe = world.recipe_lookup(bottom_oven_contents)
-		bottom_timer.set_wait_time(recipe.get_cook_time())
+	elif(bottom_oven_contents in oven_recipes and bottom_oven_off and not bottom_recipe_ready):
+		bottom_recipe = world.recipe_lookup(bottom_oven_contents)
+		bottom_timer.set_wait_time(bottom_recipe.get_cook_time())
 		bottom_timer.start()
 		bottom_oven_off = false
 	# Which timers are running?
@@ -90,11 +69,11 @@ func _process(delta):
 
 func _on_Top_Oven_Timer_timeout():
 	print("DONE")
-	recipe_ready = true
+	top_recipe_ready = true
 	top_oven_off = true
-	recipe = world.recipe_lookup(top_oven_contents)
+	top_recipe = world.recipe_lookup(top_oven_contents)
 	top_oven_contents.clear()
-	top_oven_contents.append(recipe)
+	top_oven_contents.append(top_recipe)
 	$OvenReady.show()
 	$OvenReady.play("ready")
 	top_timer.stop()
@@ -105,10 +84,10 @@ func _on_Top_Oven_Timer_timeout():
 func _on_Bottom_Oven_Timer_timeout():
 	print("DONE")
 	bottom_oven_off = true
-	recipe_ready = true
-	recipe = world.recipe_lookup(bottom_oven_contents)
+	bottom_recipe_ready = true
+	bottom_recipe = world.recipe_lookup(bottom_oven_contents)
 	bottom_oven_contents.clear()
-	bottom_oven_contents.append(recipe)
+	bottom_oven_contents.append(bottom_recipe)
 	$OvenReady.show()
 	$OvenReady.play("ready")
 	bottom_timer.stop()
@@ -126,3 +105,35 @@ func _on_Area2D_body_exited(body):
 	# check to see if that body is the player 
 	if(body.get_name() == "Player"):
 		player_near_oven = false
+
+
+func _on_Top_pressed():
+	if(player.holding.size() > 0):
+			if(typeof(player.holding[0]) == TYPE_STRING):
+				if(top_oven_off):
+					top_oven_contents.append(player.holding.pop_front())
+	elif(top_recipe_ready and player.holding.size() == 0):
+		top_oven_contents.clear()
+		player.add_object(top_recipe)
+		top_recipe_ready = false
+		$OvenReady.hide()
+	elif(not top_oven_off):
+		screen_print.append_bbcode("[color=red]The top oven is still cooking.[/color]\n")
+	$"Control/OvenPopUp".hide()
+		
+
+
+func _on_Bottom_pressed():
+	if(player.holding.size() > 0):
+			if(typeof(player.holding[0]) == TYPE_STRING):
+				if(bottom_oven_off):
+					bottom_oven_contents.append(player.holding.pop_front())
+	elif(bottom_recipe_ready and player.holding.size() == 0):
+		bottom_oven_contents.clear()
+		player.add_object(bottom_recipe)
+		bottom_recipe_ready = false
+		$OvenReady.hide()
+	elif(not bottom_oven_off):
+		screen_print.append_bbcode("[color=red]The bottom oven is still cooking.[/color]\n")
+	$"Control/OvenPopUp".hide()
+	
--- Recipe2.gd
+++ Recipe2.gd
@@ -1,12 +1,23 @@
 extends ColorRect
 var main_dish
 var side_dish
+onready var t = $"Timer"
+var active = false
 
 func _ready():
 	$"Main1".clear()
 	$"Side1".clear()
+	
+#func _process(delta):
+#	pass
+#	if(active):
+#		pass
+#		# update progess bar
+#		# check to see if time left is 0
+#			# if so, deactivate
 
 func set_main(main):
+	active = true
 	main_dish = main
 	$"Main1".clear()
 	$"Main1".append_bbcode("[center]%s[/center]" % main.get_name())
@@ -27,3 +38,9 @@ func get_main():
 
 func get_side():
 	return side_dish
+	
+func update_progress():
+	pass
+
+func reset_progress():
+	pass
